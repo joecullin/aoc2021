e89af3804488565b3d829d91fad8187971a94428
Joe Cullin <joe@joecullin.com>
2021-12-05 14:20:59 -0500

    Day 2 solutions.

--- solutions/05.2.js
+++ solutions/05.2.js
@@ -14,40 +14,29 @@ const run = async (params) => {
         };
     });
 
-    // I'm not going to worry about making this more concise until I see part 2.
-
-    // ... actually I'm going to leave it. See part 2 solution for a cleaner walkTheLine function.
-
     const walkTheLine = (line) => {
-        if (!(line.isVertical || line.isHorizontal)){
-            return;
-        }
-
         const markPoint = (x,y) => {
             const id = `${x}:${y}`;
             const val = grid[id] || 0;
             grid[id] = val + 1; 
         };
 
-        let step;
         let startPoint = line.points[0];
         let endPoint = line.points[1];
+        let xStep = startPoint.x === endPoint.x
+            ? 0
+            : (startPoint.x < endPoint.x) ? 1 : -1;
+        let yStep = startPoint.y === endPoint.y
+            ? 0
+            : (startPoint.y < endPoint.y) ? 1 : -1;
+
         let x = startPoint.x;
         let y = startPoint.y;
         markPoint(x,y);
-        if (line.isVertical){
-            step = (startPoint.y < endPoint.y) ? 1 : -1;
-            while (y !== endPoint.y){
-                y += step;
-                markPoint(x,y);
-            }
-        }
-        else if (line.isHorizontal){
-            step = (startPoint.x < endPoint.x) ? 1 : -1;
-            while (x !== endPoint.x){
-                x += step;
-                markPoint(x,y);
-            }
+        while (y !== endPoint.y || x !== endPoint.x){
+            y += yStep;
+            x += xStep;
+            markPoint(x,y);
         }
     };
 
